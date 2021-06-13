5865b84a0e9306ceaa62e089fa0dba3d1e2dc183
lelanthran <lelanthran@gmail.com>
2021-06-13 09:22:46 +0200

    Added single file to test with.

---
+++ src/gtk-cheat-sheet.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+   printf ("gtk widgets\n");
+   return 0;
+}
---
+++ src/swish.h
@@ -0,0 +1,18 @@
+
+#ifndef H_SWISH
+#define H_SWISH
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif
+
+

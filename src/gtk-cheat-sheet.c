f41d045504678ee2be7817eefa2dca7ed9acab01
lelanthran <lelanthran@gmail.com>
2021-06-13 12:53:29 +0200

    Finally got the gtk-3 example working.

--- src/gtk-cheat-sheet.c
+++ src/gtk-cheat-sheet.c
@@ -1,6 +1,5 @@
 
 #include <stdio.h>
-#include <stdlib.h>
 
 #include <gtk/gtk.h>
 
@@ -17,16 +16,21 @@ activate (GtkApplication *app,
 {
   GtkWidget *window;
   GtkWidget *button;
+  GtkWidget *button_box;
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
   gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
+  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_container_add (GTK_CONTAINER (window), button_box);
+
   button = gtk_button_new_with_label ("Hello World");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  gtk_window_set_child (GTK_WINDOW (window), button);
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
 
-  gtk_window_present (GTK_WINDOW (window));
+  gtk_widget_show_all (window);
 }
 
 int

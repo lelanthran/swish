0cbe0093f26e400d031cd4a5338681ae6a36f4cc
lelanthran <lelanthran@gmail.com>
2021-06-14 17:04:58 +0200

    Added experiments with gtk.

--- src/gtk-cheat-sheet.c
+++ src/gtk-cheat-sheet.c
@@ -3,35 +3,56 @@
 
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
+// #define INTERFACE_FILE ("test-scripts/gtk-cheat-sheet.gtk3-ui")
+#define INTERFACE_FILE ("test-scripts/glade-test.glade")
+
+void
+button_clicked (GtkButton *button,
+                gpointer   data)
 {
-  g_print ("Hello World\n");
+  g_print ("Hello World from %p/%p\n", button, data);
 }
 
 static void
 activate (GtkApplication *app,
           gpointer        user_data)
 {
-  GtkWidget *window;
+  // GtkWidget *window;
+  GtkWindow *window2;
   GtkWidget *button;
+  GtkButton *dialog_button;
+  GtkButton *quit_button;
   GtkWidget *button_box;
+  GtkBuilder *interface;
 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  interface = gtk_builder_new_from_file (INTERFACE_FILE);
+  dialog_button = GTK_BUTTON (gtk_builder_get_object (interface, "dialog_button"));
+  quit_button = GTK_BUTTON (gtk_builder_get_object (interface, "quit_button"));
+  window2 = GTK_WINDOW (gtk_builder_get_object (interface, "FWindow"));
+
+  printf ("interface gave us %p / %p\n", dialog_button, window2);
+
+  // window = gtk_application_window_new (app);
+  // gtk_window_set_title (GTK_WINDOW (window), "Window");
+  // gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
 
   button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
+  // gtk_container_add (GTK_CONTAINER (window), button_box);
 
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  button = gtk_button_new_with_label ("MyButton");
+  g_signal_connect (dialog_button, "clicked", G_CALLBACK (button_clicked), dialog_button);
+  g_signal_connect (quit_button, "clicked", G_CALLBACK (button_clicked), quit_button);
+  g_signal_connect_swapped (quit_button, "clicked", G_CALLBACK (gtk_widget_destroy), window2);
   gtk_container_add (GTK_CONTAINER (button_box), button);
 
-  gtk_widget_show_all (window);
+  // gtk_widget_show_all (window);
+  gtk_application_add_window (app, window2);
+  gtk_widget_show_all (GTK_WIDGET (window2));
+
+  gtk_widget_destroy (interface);
+  g_object_unref (interface);
 }
+
 
 int
 main (int    argc,
